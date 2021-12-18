70f2fcbddca66470cb4247964a73116ac2030450
AXOME <90136144+AXOMEstudios@users.noreply.github.com>
2021-12-18 09:53:13 +0000

    moved version file into updater.py

--- updater.py
+++ updater.py
@@ -3,16 +3,14 @@ import requests
 VERSION_NUMBER_URL = "https://raw.githubusercontent.com/AXOMEstudios/nq-cli/master/data/nq-version"
 SOFTWARE           = "nq-cli"
 HOW_TO             = "clone the repository at https://github.com/AXOMEstudios/nq-cli into nq-cli"
+CURRENT            = "1.01"
 
 def checkForUpdate():
   version = requests.get(VERSION_NUMBER_URL).text
-  with open("version") as f:
-    current = f.read()
-    f.close()
-  if version != current:
-    return (True, current, version)
+  if version != CURRENT:
+    return (True, CURRENT, version)
   else:
-    return (False, current, version)
+    return (False, CURRENT, version)
 
 def generateMessage(current, new):
   return f'''Warning!
